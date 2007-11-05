e615b3bb9fccd6f3a6617693ca3f14879d07fe3c
cch1 <cch1@b5fa333d-9bf8-45b9-b2f2-2709d1971210>
2007-11-05 14:06:31 +0000

    Strip comments and profiles from thumbnails to further reduce size.
    git-svn-id: svn://cho.hapgoods.com/groupsmarts/attachment_fu/trunk@30 b5fa333d-9bf8-45b9-b2f2-2709d1971210

--- lib/technoweenie/attachment_fu/processors/rmagick_processor.rb
+++ lib/technoweenie/attachment_fu/processors/rmagick_processor.rb
@@ -32,6 +32,7 @@ module Technoweenie # :nodoc:
             resize_image_or_thumbnail! img
             self.width  = img.columns if respond_to?(:width)
             self.height = img.rows    if respond_to?(:height)
+            img.strip! if self.thumbnail?
             callback_with_args :after_resize, img
           end if image?
         end
