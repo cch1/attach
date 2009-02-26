88404c7b73a20a0120768a14359918fdc35cca27
Chris Hapgood <cch1@hapgoods.com>
2009-02-26 18:02:17 -0500

    Fleshed out Sources::File class

--- lib/group_smarts/attach/sources/file.rb
+++ lib/group_smarts/attach/sources/file.rb
@@ -3,25 +3,56 @@ module GroupSmarts # :nodoc:
     module Sources
       # Methods for duplexed File sources/sinks.
       class File < GroupSmarts::Attach::Sources::IO
-        BASE_URI = ::URI.parse('file://localhost/')
-        def initialize(file, metadata)
+        def initialize(uri, metadata = {})
+          @uri = uri
           super
-          @file = @data
-          @io = @file
         end
         
+        def store(source)
+          @metadata = source.metadata
+          ::File.cp(source.tempfile.path, fn)
+        end
         # =Metadata=
         # Construct a URI using the file scheme.
         def uri
-          @uri ||= BASE_URI.merge(URI.encode(@file.path)).normalize
+          @uri
+        end
+        
+        def destroy
+          begin
+            FileUtils.rm fn
+          rescue
+            logger.info "Exception destroying  #{fn.inspect}: [#{$!.class.name}] #{$1.to_s}"
+          ensure
+            super
+          end
         end
 
         # =Data=
+        def blob
+          io.read
+        end
+
         # Returns a closed Tempfile of source's data.
         def tempfile
           returning Tempfile.new(filename, GroupSmarts::Attach.tempfile_path) do |tmp|
             tmp.close
             FileUtils.cp file, tmp.path
+          end
+        end
+
+        def io
+          file
+        end
+        
+        private
+        def fn
+          @fn ||= "#{@uri.path}.#{mime_type.to_sym}"
+        end
+
+        def file
+          returning (@file || ::File.open(fn)) do |f|
+            f.rewind
           end
         end
       end
