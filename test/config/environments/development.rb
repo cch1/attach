6fcb5303c92c69c6cec09581bb848e8253f59e0e
Chris Hapgood <cch1@hapgoods.com>
2010-06-11 18:00:12 -0400

    Added development environment initialization

---
+++ test/config/environments/development.rb
@@ -0,0 +1,6 @@
+config.after_initialize do
+  require 'db/schema.rb'
+  require 'active_record/fixtures'
+  Fixtures::FILE_STORE = File.join(RAILS_ROOT, 'test', 'fixtures', 'attachments')
+  Fixtures.create_fixtures('test/fixtures', [:attachments, :attachment_blobs, :users], :attachment_blobs => Hapgood::Attach::AttachmentBlob)
+end
