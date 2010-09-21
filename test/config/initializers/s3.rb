3e7458ab6f5bd173d56ac7992d746185318f136a
Chris Hapgood <cch1@hapgoods.com>
2010-09-21 12:10:59 -0400

    Change S3 credentials

--- test/config/initializers/s3.rb
+++ test/config/initializers/s3.rb
@@ -1,5 +1,5 @@
 ::Hapgood::Attach::Sources::S3.config.merge!(
-  :credentials => {:secret_access_key => "v0xaN0sJ1htcnJcPoEqhTwdXdDJlPDrvsfgwXpx0", :access_key_id => "AKIAI2L74WCSSNCGL7VQ"},
+  :credentials => {:secret_access_key => "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", :access_key_id => "AKIAJAB7ETCUXOW3OWKA"},
   :access => :public_read
 )
 Hapgood::Attach::Sources::S3.establish_connection!
